34866c644d27062b553482e2e9d84d2a4af0a10b
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-12-07 16:46:57 +0000

    latest build

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ const callGenerateEndpoint = async () => {
             <h1>Find a Special Gift this Christmas 🎄</h1>
           </div>
           <div className="header-subtitle">
-            <h2> ( e.g. Give me a creative list of gift ideas for my co worker. We work in HR together. Give me a creative gift idea for my 18-year-old son for Christmas. He likes sports and hanging out with his friends. )
+            <h2> ( e.g. Give me a creative list of gift ideas for my co worker. We work in HR together / Give me a creative gift idea for my 18-year-old son for Christmas. He likes sports and hanging out with his friends. )
             </h2>
           </div>
         </div>
