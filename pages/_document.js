8e23148d340f1e316922776763ea7661db0e350e
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-12-07 18:13:54 +0000

    latest build

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta property="og:description" content="Find a Special Gift this Christmas" key="description"/>
         <meta
           property="og: image"
-          content="/Users/GPT3 Project/Personal-Shopper/solen-feyissa-LBNJi8qHIbA-unsplash.jpeg"
+          content="/Users/GPT3 Project/Personal-Shopper/nguyen-dang-hoang-nhu-4LiIv59mw-4-unsplash.jpeg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
