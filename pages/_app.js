ca3d1d21b1ec5b592dc4fc6937aab851bc227184
Harry Arkwright <84912003+HarryArkwright123@users.noreply.github.com>
2022-12-07 17:04:36 +0000

    latest build

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import './styles.css';
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
-export default App;
+
+export default MyApp;
